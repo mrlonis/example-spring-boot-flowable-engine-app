3f93d1977df7a4ab307e1da440a7c7e11ce3e03d
Matthew Lonis <mattlonis@gmail.com>
2025-04-01 21:19:59 -0400

    Adding Spotless

--- src/test/java/com/mrlonis/flowable/example/ExampleSpringBootFlowableEngineAppApplicationTests.java
+++ src/test/java/com/mrlonis/flowable/example/ExampleSpringBootFlowableEngineAppApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class ExampleSpringBootFlowableEngineAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
